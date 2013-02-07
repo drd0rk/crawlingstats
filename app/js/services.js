fea6fbaed4fc2346799c03361b80d45807797361
Stefan Bilharz <s.bilharz@web.de>
2013-02-07 19:33:37 +0100

    Adjustments to new cachedResource API

--- app/js/services.js
+++ app/js/services.js
@@ -8,41 +8,44 @@
       return moment;
     }).
     factory("matches", ["cachedResource", "users", function (cachedResource, users) {
-      return cachedResource("/matches", {
-        afterRead: function (match) {
-          if (typeof match.team1.defensive === "string") {
-            match.team1.defensive = users.get({id: match.team1.defensive});
+      return cachedResource({
+        path: "/matches",
+        hooks: {
+          afterRead: function (match) {
+            if (typeof match.team1.defensive === "string") {
+              match.team1.defensive = users.get({id: match.team1.defensive});
+            }
+            if (typeof match.team1.offensive === "string") {
+              match.team1.offensive = users.get({id: match.team1.offensive});
+            }
+            if (typeof match.team2.defensive === "string") {
+              match.team2.defensive = users.get({id: match.team2.defensive});
+            }
+            if (typeof match.team2.offensive === "string") {
+              match.team2.offensive = users.get({id: match.team2.offensive});
+            }
+            return match;
+          },
+          beforeWrite: function (match) {
+            if (typeof match.team1.defensive === "object") {
+              match.team1.defensive = match.team1.defensive._id;
+            }
+            if (typeof match.team1.offensive === "object") {
+              match.team1.offensive = match.team1.offensive._id;
+            }
+            if (typeof match.team2.defensive === "object") {
+              match.team2.defensive = match.team2.defensive._id;
+            }
+            if (typeof match.team2.offensive === "object") {
+              match.team2.offensive = match.team2.offensive._id;
+            }
+            return match;
           }
-          if (typeof match.team1.offensive === "string") {
-            match.team1.offensive = users.get({id: match.team1.offensive});
-          }
-          if (typeof match.team2.defensive === "string") {
-            match.team2.defensive = users.get({id: match.team2.defensive});
-          }
-          if (typeof match.team2.offensive === "string") {
-            match.team2.offensive = users.get({id: match.team2.offensive});
-          }
-          return match;
-        },
-        beforeWrite: function (match) {
-          if (typeof match.team1.defensive === "object") {
-            match.team1.defensive = match.team1.defensive._id;
-          }
-          if (typeof match.team1.offensive === "object") {
-            match.team1.offensive = match.team1.offensive._id;
-          }
-          if (typeof match.team2.defensive === "object") {
-            match.team2.defensive = match.team2.defensive._id;
-          }
-          if (typeof match.team2.offensive === "object") {
-            match.team2.offensive = match.team2.offensive._id;
-          }
-          return match;
         }
       });
     }]).
     factory("users", ["cachedResource", function (cachedResource) {
-      return cachedResource("/users/:id");
+      return cachedResource({path: "/users/:id"});
     }]);
 
 }());
